07221ca9246ed8299e47e1908fb24be9f8d0953c
Tanaca <joao.ramos@evtit.com>
2022-01-21 00:56:41 -0300

    💄 add title page and icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
         return (
             <Html lang="pt-BR">
                 <Head>
-                    <link rel="shortcut icon" href="images/favicon.png" />
+                    <link rel="shortcut icon" href="favicon.png" />
                     <link
                         rel="preconnect"
                         href="https://fonts.googleapis.com"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { AssetResponse, getAssetCards, getAssetCardsUrl } from "lib/asset";
 import type { NextPage } from "next";
 import { useMemo } from "react";
 import { useTheme as useThemeStyled } from "styled-components";
+import Head from "next/head";
 
 const Home: NextPage<{ cards: AssetResponse[] }> = ({ cards }) => {
     const { data } = useFetch<{ data: AssetResponse[] }>(
@@ -23,17 +24,22 @@ const Home: NextPage<{ cards: AssetResponse[] }> = ({ cards }) => {
     );
 
     return (
-        <div className="container mx-auto mt-10 px-4">
-            <Text className="font-normal text-2xl" color="lightText">
-                NFTs
-            </Text>
-            <div className="w-full mt-6 mb-4 flex-">
-                <button>
-                    <Icon name="grid" color={sortButtonColor} size={13.5} />
-                </button>
+        <>
+            <Head>
+                <title>Fake Market</title>
+            </Head>
+            <div className="container mx-auto mt-10 px-4">
+                <Text className="font-normal text-2xl" color="lightText">
+                    NFTs
+                </Text>
+                <div className="w-full mt-6 mb-4 flex-">
+                    <button>
+                        <Icon name="grid" color={sortButtonColor} size={13.5} />
+                    </button>
+                </div>
+                <Cards cards={data?.data} />
             </div>
-            <Cards cards={data?.data} />
-        </div>
+        </>
     );
 };
 
